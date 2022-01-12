d1080d1c1d3d9db3e7589fd879ae5b48dcca1ce0
Lost Octet <lost.octet@gmail.com>
2022-01-12 23:05:22 +0530

    first commit

---
+++ first_code.py
@@ -0,0 +1,12 @@
+# Python is a dynamically typed Languages. Some data types are as follows-
+
+# Numbers type are defined as class int, float and complex
+a = 5
+print(a, "is of type", type(a))
+
+a = 2.0
+print(a, "is of type", type(a))
+
+a = 1+2j
+print(a, "is complex number?", isinstance(1+2j, complex))
+print('Hello World')
